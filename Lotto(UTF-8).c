ae0c1c20f0838334ea3314a1af5cb493561c2bc7
rena <dayoungrenakang@gmail.com>
2023-04-14 17:37:22 +0900

    no message

--- Lotto(UTF-8).c
+++ Lotto(UTF-8).c
@@ -3,62 +3,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 6
+#define RANGE 45
 
-void random_numbers(int*arr, int range) {
-	int i, j;
-	int RANGE = 45;
-	//랜덤의 숫자를 6개 중복없이 뽑기 
-	for (i = 0; i < RANGE ; i++) { //1부터 45까지의 숫자 배열 numbers 생성
-		arr[i] = i + 1; //i 가 0부터 44까지 증가하면서 i + 1값을 배열에 저장
-	}
-
-	// numbers 배열을 무작위로 섞기 (Fisher-Yates shuffle 방법)
-	for (i = 0; i < RANGE ; i++) { //i가 0부터 44까지 증가하면서
-		j = rand() % RANGE; //랜덤의 정수를 생성하여 j에 저장한다
-		int temp = arr[i];
-		arr[i] = arr[j]; //numbers[i] 값과 numbers[j]값을 서로 교환한다.
-		arr[j] = temp;
-	}
-}
-
-void selection_sort(int*arr, int MAX) {
-	
-	int max = 6;
-	//선택정렬 알고리즘 이용하여 오름차순 정렬  
-	int i, j, min_idx, temp;
-	for (i = 0; i < max - 1; i++) {     // i는 0부터 5까지 1번째 씩 증가하면서  
-    	min_idx = i;                    // min_idx라는 변수에 i번째를 일단 저장함 
-    	for (j = i + 1; j < max; j++) {   // i번호의 하나 다음 수인 j 번째 와 비교한다. 
-        	if (arr[j] < arr[min_idx]) { //j번째 숫자와 min_idx의 숫자를 비교해서 min_idx가 크다면  
-                min_idx = j;                   //min_idx는 j번째가 된다. 
-        	}
-    	}
-    	temp = arr[min_idx];
-    	arr[min_idx] = arr[i];
-    	arr[i] = temp;
-	}
-}
+void random_select(int *numbers);
+void sort_numbers(int *numbers);
 
 int main() {
-	int i;
-	int numbers[6];
-	random_numbers(numbers, range);
-	printf("숫자 : ");
-
-
-	for (i = 0; i < max; i++) {
-	    printf("%d ", numbers[i]);
-	}
-	printf("\n");
+	int numbers[MAX];
 	
-	selection_sort(numbers, max);
-	printf("오름차순 정렬 : ");
-	for (i = 0; i < max; i++) {
-	    printf("%d ", numbers[i]);
-	}
-	printf("\n");
+	//랜덤 숫자 발생을 위해 사용되는 함수 srand() 무작위 수열을 초기화하기 위한 시드 값 설정 
+	//time()함수는 현재 시간을 초 단위로 반환. 이를 이용하여 시드값 설정하면 매번 다른 랜덤숫자 
+	srand((unsigned int) time(NULL));
+	
+	random_select(numbers);
+	sort_numbers(numbers);
 	
 	return 0;
 }
 
+void random_select(int *numbers) {
+	
+	int i, j, temp;
+	
+	//1부터 45까지의 숫자 배열 numbers 생성 
+   for (i = 0; i < RANGE ; i++) {  
+   		numbers[i] = i + 1;		 
+   }
+   
+   // numbers 배열을 무작위로 섞기 (Fisher-Yates shuffle 방법)
+   for (i = 0; i < RANGE ; i++) {	 	//i가 0부터 44까지 증가하면서  
+   		j = rand() % RANGE; 		//랜덤의 정수를 생성하여 j에 저장한다 
+   		temp = numbers[i];			
+   		numbers[i] = numbers[j];	//numbers[i] 값과 numbers[j]값을 서로 교환한다. 
+   		numbers[j] = temp;			
+   }
+   
+    printf("숫자 : ");
+	for (i = 0; i < MAX; i++) {
+		printf("%d ", numbers[i]);
+	} 
+	printf("\n");
+}
 
+void sort_numbers(int *numbers) {
+	int i, j, min_idx, temp;
+	
+	 // 선택정렬 알고리즘 이용하여 오름차순 정렬
+	 for(i = 0; i < MAX - 1; i++){
+		min_idx = i;
+		for(j = i + 1; j < MAX; j++){
+			if(numbers[j] < numbers[min_idx]){
+				min_idx = j;
+			}
+		}
+		temp = numbers[min_idx];
+		numbers[min_idx] = numbers[i];
+		numbers[i] = temp;
+	} 
+	
+	printf("오름차순 정렬 : ");
+	for(i = 0; i < MAX; i++){
+		printf("%d ", numbers[i]);
+	} 
+	printf("\n");
+}
